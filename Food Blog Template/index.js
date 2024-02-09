34164e191e047f2b2b970cc8dadbac1366c7acd3
MG Rahul <153736504+mgrahul639@users.noreply.github.com>
2024-02-10 00:56:29 +0600

    Add files via upload

---
+++ Food Blog Template/index.js
@@ -0,0 +1,8 @@
+
+
+const navBarIcon = document.querySelector('.navbar_icon');
+
+navBarIcon.addEventListener('click', () =>{
+    
+	document.querySelector('.navbar').classList.toggle('active');
+});
